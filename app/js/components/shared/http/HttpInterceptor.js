cec3573c2e8f26578dc3f6e77d1521efbe48cfe8
Ludovic Soltys <ludovic.soltys@gmail.com>
2017-01-08 22:01:33 +0100

    some refactoring

--- app/js/components/shared/http/HttpInterceptor.js
+++ app/js/components/shared/http/HttpInterceptor.js
@@ -1,12 +1,13 @@
-function HttpInterceptor($q) {
+function HttpInterceptor($q, $injector) {
   this.q = $q;
+  this.injector = $injector;
 }
 
 HttpInterceptor.prototype =  {
 
     responseError : function responseError(response) {
         if (response.status == 401){
-            window.location = "/login";
+          this.injector.get('$state').transitionTo('login');
         }
         return this.q.reject(response);
     }
--- app/js/components/shared/http/http.config.js
+++ app/js/components/shared/http/http.config.js
@@ -1,5 +1,5 @@
 angular.module('app.http', [])
   .service('httpInterceptor', ['$q', HttpInterceptor])
-  .config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('httpInterceptor');
+  .config(['$httpProvider', 'httpInterceptor', function($httpProvider, httpInterceptor) {
+    $httpProvider.interceptors.push(httpInterceptor);
   }]);
